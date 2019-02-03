70b68983afc6f3c705fc69604657f3be4cea3e1e
VictoriyaRoy <victoriyaroi2003@gmail.com>
2019-02-03 20:14:13 +0200

    ...

--- app/src/main/java/com/example/user/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication3/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.myapplication3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 
@@ -11,12 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar mActionBarToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mActionBarToolbar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 }
